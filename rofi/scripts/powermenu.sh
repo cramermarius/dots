c62af162d0b5b6dc28a9195f91c42c7d2dab8528
Marius Cramer <marcramer@pm.me>
2020-02-01 06:55:41 +0100

    change powermenu layout, add lock option

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
 # options to be displayed
-option1="logout"
-option2="suspend"
-option3="scheduled suspend (10min)"
-option4="scheduled suspend (20min)"
-option5="scheduled suspend (30min)"
-option6="scheduled suspend (45min)"
-option7="scheduled suspend (60min)"
+option0="suspend"
+option1="scheduled suspend (10min)"
+option2="scheduled suspend (20min)"
+option3="scheduled suspend (30min)"
+option4="scheduled suspend (45min)"
+option5="scheduled suspend (60min)"
+option6="lock"
+option7="logout"
 option8="reboot"
 option9="shutdown"
 
 # options passed into variable
-options="$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7\n$option8\n$option9"
+options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7\n$option8\n$option9"
 
-chosen="$(echo -e "$options" | rofi -lines 9 -dmenu -p "power")"
+chosen="$(echo -e "$options" | rofi -lines 10 -dmenu -p "power")"
 case $chosen in
-    $option1)
-        i3-msg exit;;
-    $option2)
+	$option0)
         systemctl suspend && i3lock -c 4c566a -n;;
-	$option3)
+    $option1)
 		sleep 600 && systemctl suspend && i3lock -c 4c566a -n;;
-	$option4)
+    $option2)
 		sleep 1200 && systemctl suspend && i3lock -c 4c566a -n;;
-	$option5)
+	$option3)
 		sleep 1800 && systemctl suspend && i3lock -c 4c566a -n;;
-	$option6)
+	$option4)
 		sleep 2700 && systemctl suspend && i3lock -c 4c566a -n;;
-	$option7)
+	$option5)
 		sleep 3600 && systemctl suspend && i3lock -c 4c566a -n;;
+	$option6)
+		i3lock -c 4c566a -n;;
+	$option7)
+        i3-msg exit;;
     $option8)
         systemctl reboot;;
 	$option9)
